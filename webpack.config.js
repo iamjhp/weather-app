9f1436de345cda1ec05ac79b88f0e9eb4d000ec4
iamjhp <jooho.park@outlook.com>
2022-04-20 22:08:52 +0200

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+    {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+    },
+    {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    },
+    ],
+  },
+  watchOptions: {
+    poll: true
+  },
+};
